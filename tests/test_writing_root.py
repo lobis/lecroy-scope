55d5b3b271db078041a15736894b47d00f335985
Luis Obis <luis.antonio.obis@gmail.com>
2022-11-11 22:17:03 +0100

    add test for additional arguments for root tree

--- tests/test_writing_root.py
+++ tests/test_writing_root.py
@@ -10,13 +10,18 @@ files_path = Path(__file__).parent / "files"
 
 
 def test_writing_tree(tmp_path):
-    for file, multiply in zip(
-        [
-            files_path / "pulse.trc",
-            files_path / "pulse_sequence.trc",
-            files_path / "pulse_sequence.trc",
-        ],
-        [False, False, True],
+    for i, (file, multiply) in enumerate(
+        zip(
+            [
+                files_path / "pulse.trc",
+                files_path / "pulse.trc",
+                files_path / "pulse_sequence.trc",
+                files_path / "pulse_sequence.trc",
+                files_path / "pulse_sequence.trc",
+            ],
+            [False, False, False, True, False],
+            strict=True,
+        )
     ):
         if not multiply:
             trace_group = lecroyscope.TraceGroup(lecroyscope.Trace(file, channel=2))
@@ -31,6 +36,15 @@ def test_writing_tree(tmp_path):
             assert branches == lecroyscope.writing.root.get_tree_branch_definitions(
                 next(trace_group)
             )
+
+        use_additional_info = i == 1 or i == 3
+        if use_additional_info:
+            branches = lecroyscope.writing.root.get_tree_branch_definitions(
+                trace_group, additional_float=float, additional_int=int
+            )
+            additional_float = 1.25
+            additional_int = 137
+
         tree_name = "t"
         uproot_file = tmp_path / "pulse.root"
         with uproot.recreate(uproot_file) as f:
@@ -39,7 +53,13 @@ def test_writing_tree(tmp_path):
                 branches,
                 "Tree",
             )
-            data = lecroyscope.writing.root.get_tree_data(trace_group)
+            data = lecroyscope.writing.root.get_tree_extend_data(trace_group)
+            if use_additional_info:
+                data = lecroyscope.writing.root.get_tree_extend_data(
+                    trace_group,
+                    additional_float=additional_float,
+                    additional_int=additional_int,
+                )
             tree.extend(data)
 
         with uproot.open(uproot_file) as f:
@@ -49,6 +69,15 @@ def test_writing_tree(tmp_path):
                 channel: tree[f"CH{channel:d}"].array()
                 for channel in trace_group.channels
             }
+            if use_additional_info:
+                additional_float_file = tree["additional_float"].array()
+                additional_int_file = tree["additional_int"].array()
+                assert_array_equal(
+                    additional_float_file, len(times) * [additional_float]
+                )
+                assert_array_almost_equal(
+                    additional_int_file, len(times) * [additional_int]
+                )
 
         # check all times are equal
         for time in times:
