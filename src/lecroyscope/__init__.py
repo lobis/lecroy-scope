d2e8a6dc7aad6166afce3a96a93d5f5817e12329
Luis Obis <luis.antonio.obis@gmail.com>
2022-11-11 17:22:03 +0100

    add support (optional) to save data into root tree using uproot

--- src/lecroyscope/__init__.py
+++ src/lecroyscope/__init__.py
@@ -1,2 +1,7 @@
 from .reading import Trace, TraceGroup
 from .control import Scope
+
+try:
+    from .writing import root
+except ImportError:
+    pass
---
+++ src/lecroyscope/writing/root/__init__.py
@@ -0,0 +1,54 @@
+from __future__ import annotations
+
+import uproot
+import numpy
+from lecroyscope import Trace, TraceGroup
+
+
+def get_tree_branch_definitions(
+    trace_or_group: Trace | TraceGroup, additional_branches: dict = None
+) -> dict:
+    if isinstance(trace_or_group, TraceGroup):
+        trace_group = trace_or_group
+    elif isinstance(trace_or_group, Trace):
+        trace_group = TraceGroup(trace_or_group)
+    else:
+        raise ValueError(
+            "Input argument should be either 'lecroyscope.Trace' or 'lecroyscope.TraceGroup'"
+        )
+    if trace_group.time is None:
+        raise ValueError(
+            "Trace Group does not have unified time information (probably do not come from the same trigger)"
+        )
+    channels = {
+        f"CH{trace.channel}": ("f4", (len(trace_group.time),)) for trace in trace_group
+    }
+    branches = {
+        "time": ("f4", (len(trace_group.time),)),
+        **channels,
+    }
+    if additional_branches is not None:
+        for key in additional_branches.keys():
+            if not isinstance(key, str):
+                raise ValueError(f"key '{key}' should be a 'str'")
+        branches = {**branches, **additional_branches}
+    return branches
+
+
+def get_tree_data(trace_or_group: Trace | TraceGroup):
+    if isinstance(trace_or_group, TraceGroup):
+        trace_group = trace_or_group
+    elif isinstance(trace_or_group, Trace):
+        print("creating trace group")
+        trace_group = TraceGroup(trace_or_group)
+        print("finished trace group")
+    else:
+        raise ValueError(
+            "Input argument should be either 'lecroyscope.Trace' or 'lecroyscope.TraceGroup'"
+        )
+
+    channels = {f"CH{trace.channel}": trace.voltage for trace in trace_group}
+    return {
+        "time": numpy.array(trace_group.trace_length * [trace_group.time]),
+        **channels,
+    }
